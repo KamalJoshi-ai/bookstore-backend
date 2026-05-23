2976aed2a5637150fbc4240f9fe5826bdc43c301
KamalJoshi-ai <kamaljoshi13524@gmail.com>
2026-05-23 12:50:10 +0530

    final commit

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -3,13 +3,13 @@ import { Response } from "express";
 export const clearAuthCookies=(res:Response)=>{
   res.clearCookie("access_token",{
     httpOnly:true,
-    secure:process.env.NODE_ENV==="production",
+    secure:true,
     sameSite:"none"
   });
 
   res.clearCookie("refresh_token",{
     httpOnly:true,
-    secure:process.env.NODE_ENV==="production",
+    secure:true,
     sameSite:"none"
   });
 };
