630f9edb10447673e046d2b942079c5d4308a3a7
KamalJoshi-ai <kamaljoshi13524@gmail.com>
2026-05-23 14:01:19 +0530

    final commit

--- config/emailConfig.ts
+++ config/emailConfig.ts
@@ -1,13 +1,18 @@
 
 import nodemailer from "nodemailer";
+import dns from "dns";
 
-import crypto from 'crypto'
+dns.setDefaultResultOrder("ipv4first");
+
 import dotenv from 'dotenv'
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service:"gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
